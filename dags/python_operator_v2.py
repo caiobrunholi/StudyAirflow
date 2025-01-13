3181cea46d449cac0e76b8aeb84257e4f671e8dd
caiobrunholi <caio.brunholi@hotmail.com>
2025-01-13 09:32:03 -0300

    feat(python): add python operator code

---
+++ dags/python_operator_v2.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task
+import pendulum
+import pprint
+
+with DAG(
+    dag_id="python_operator_v1",
+    schedule="* * * * *",
+    start_date=pendulum.datetime(2023, 6, 22, 14, 10, tz="America/Sao_Paulo"),
+    catchup=True
+) as dag:
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+    
+    @task(task_id="python")
+    def meu_codigo(ds=None, **kwargs):
+        pprint.pprint(kwargs)
+        print(ds)
+        return "Meu retorno agora é esse texto"
+
+    python = meu_codigo()
+
+start >> python >> end
